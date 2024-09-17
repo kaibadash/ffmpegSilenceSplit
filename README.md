8c727e0170ec94bf927abe684c0ab02fe4154cb1
kaiba <kaibadash@gmail.com>
2024-09-18 08:51:58 +0900

    Support macOS

--- README.md
+++ README.md
@@ -4,6 +4,13 @@
 
 `ffmpegSilenceSplit` is a Bash script that utilizes FFmpeg to automatically detect silent segments in an audio or video file and split the file accordingly. The script allows users to define the silence threshold and duration, making it flexible for various use cases such as removing pauses from podcasts, lectures, or other media files. By leveraging the power of FFmpeg's silencedetect filter, the script efficiently identifies and cuts out silent parts, simplifying the media editing process.
 
+## Setup
+
+```bash
+brew install grep # gnu grep for mac
+brew install ffmpeg
+```
+
 ## How to use
 
 ```bash
--- ffmpeg_silence_split.sh
+++ ffmpeg_silence_split.sh
@@ -18,6 +18,12 @@ min_duration=30
 silence_level="-30dB"
 silence_duration_sec="0.5"
 
+if command -v ggrep >/dev/null 2>&1; then
+    GREP_COMMAND="ggrep"
+else
+    GREP_COMMAND="grep"
+fi
+
 # Detect silence and store the result in the log file
 ffmpeg -i "$input_file" -af silencedetect=noise=$silence_level:d=$silence_duration_sec -f null - 2> "$silence_log"
 
@@ -37,7 +43,7 @@ calculate_duration() {
 while read -r line; do
     if [[ $line == *"silence_start:"* ]]; then
         # Get the start time of the silence
-        silence_start=$(echo $line | grep -oP 'silence_start: \K[0-9.]+')
+        silence_start=$(echo $line | $GREP_COMMAND -oP 'silence_start: \K[0-9.]+')
 
         # Calculate the duration of the segment
         duration=$(calculate_duration "$start_time" "$silence_start")
@@ -61,7 +67,7 @@ while read -r line; do
         start_time=$silence_start
     elif [[ $line == *"silence_end:"* ]]; then
         # Get the end time of the silence
-        silence_end=$(echo $line | grep -oP 'silence_end: \K[0-9.]+')
+        silence_end=$(echo $line | $GREP_COMMAND -oP 'silence_end: \K[0-9.]+')
         start_time=$silence_end  # Set the start time after the silence ends
     fi
 done < "$silence_log"
@@ -74,6 +80,9 @@ last_segment_duration=$(calculate_duration "$start_time" "$total_duration")
 # Only split the last segment if its duration is greater than the minimum duration (60 seconds)
 if (( last_segment_duration >= min_duration )); then
     output_file=$(printf "%s_%03d.m4a" "$input_filename" "$index")
+    if [ -z "$start_time" ]; then
+      start_time=0
+    fi
     ffmpeg -i "$input_file" -ss "$start_time" -c copy "$output_file"
     echo "Output: $output_file (Start: $start_time, End: End of File, Duration: $last_segment_duration seconds)"
 else
